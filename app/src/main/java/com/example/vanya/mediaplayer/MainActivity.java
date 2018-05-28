a7cf1469f10491eeb192443a3053d1096f18259c
VanyaHuaman <vanyahuaman@gmail.com>
2018-05-27 22:59:20 -0500

    initial

---
+++ app/src/main/java/com/example/vanya/mediaplayer/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.vanya.mediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.bensound_summer);
+
+        Button startButton = findViewById(R.id.startButton);
+        Button pauseButton = findViewById(R.id.pauseButton);
+        Button stopButton = findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+            }
+        });
+
+    }
+}
